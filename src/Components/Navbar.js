5f9264d9a613a4286ca3d6bee4ae152a8daf7344
TomWai821 <waicheung821@gmail.com>
2023-08-10 10:19:22 +0800

    change to dropdown menu from nav menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,18 +15,26 @@ export class Navbar extends Component{
                     </button>
                     <div className='collapse navbar-collapse' id='navbarSupportedContent'>
                         <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                            <li className='nav-item'>
-                            <a className='nav-link active' aria-current='page' href='/'>Home</a>
-                            </li>
 
-                            <li className='nav-item'><Link className='nav-link' to='/business'>Business</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/entertainment'>Entertainment</Link></li>
+                          <li className='nav-item'>
+                          <a className='nav-link active' aria-current='page' href='/'>Home</a>
+                          </li>
 
-                            <li className='nav-item'><Link className='nav-link' to='/general'>General</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/health'>Health</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/science'>Science</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/sports'>Sports</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/technology'>Technology</Link></li>
+                          <li className='nav-item dropdown'>
+                          <a className='nav-link dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false' >
+                          Title
+                          </a>
+                            <ul className='dropdown-menu' data-bs-theme='dark'>
+                              <li className='dropdown-item'><Link className='nav-link' to='/business'>Business</Link></li>
+                              <li className='dropdown-item'><Link className='nav-link' to='/entertainment'>Entertainment</Link></li>
+
+                              <li className='dropdown-item'><Link className='nav-link' to='/general'>General</Link></li>
+                              <li className='dropdown-item'><Link className='nav-link' to='/health'>Health</Link></li>
+                              <li className='dropdown-item'><Link className='nav-link' to='/science'>Science</Link></li>
+                              <li className='dropdown-item'><Link className='nav-link' to='/sports'>Sports</Link></li>
+                              <li className='dropdown-item'><Link className='nav-link' to='/technology'>Technology</Link></li>
+                            </ul>
+                          </li>
                         </ul>
                     </div>
                 </div>
--- src/Components/News.js
+++ src/Components/News.js
@@ -32,15 +32,16 @@ export class News extends Component{
             totalResults: 0,
             apiKey: 'f5f545c7bbb743e596b82a5db9abad91'
         }
-        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - MyNews`;
     }
-
+    
     async updateNews(){
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.state.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({loading: true});
         let data = await fetch(url);
         let parseData = await data.json();         // change to json file
-        this.setState({articles: parseData.articles, 
+        this.setState({
+            articles: parseData.articles, 
             totalResults: parseData.totalResults,
             loading: false});
     }
@@ -63,8 +64,8 @@ export class News extends Component{
                 loading: false
             })
         }*/
-        this.setState({page: this.state.page - 1});
-        this.updateNews();
+        this.setState({page: this.state.page - 1})
+        this.updateNews()
     }
 
     handleNextClick = async () =>{
@@ -81,34 +82,59 @@ export class News extends Component{
                 loading: false
             })
         }*/
-        this.setState({page: this.state.page + 1});
-        this.updateNews();
+        this.setState({page: this.state.page + 1})
+        this.updateNews()
+    }
+
+    fetchMoreData = async() => {
+        this.setState({page: this.state.page + 1})
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.state.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true});
+        let data = await fetch(url);
+        let parseData = await data.json();         // change to json file
+        this.setState({
+            articles: this.state.articles.concat(parseData.articles), 
+            totalResults: parseData.totalResults,
+            loading: false
+        });
     }
 
     render(){
         return(
-            <div className="container my-3">
+            <>
                 <h1 className='text-center'>My News - Top Headlines from {this.capitalizeFirstLetter(this.props.category)} category </h1>
                 {/*{this.state.loading && <Spinner/>}*/}
+                <InfiniteScroll
+                    dataLength = {this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner/>}
+                    >
 
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className='col-md-4' key={element.url}>
-                            <NewsItem title={element.title? element.title.slice(0,45) : ""} 
-                              description={element.description? element.description.slice(0,88) : ""} 
-                              imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
-                        </div>
-                    })}
+                <div className='container'>
+                    <div className="row">
+                        {!this.state.loading && this.state.articles.map((element) => {
+                            return <div className='col-md-4' key={element.url}>
+                                <NewsItem title={element.title? element.title.slice(0,45) : ""} 
+                                description={element.description? element.description.slice(0,88) : ""} 
+                                imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
+                            </div>
+                        })}
+                    </div>
                 </div>
+                </InfiniteScroll>
+
+                {/*
                 <div className='container d-flex justify-content-between'>
-                    {/* &larr; = left arrow, &rarr; = right arrow */}
+                    &larr; = left arrow, &rarr; = right arrow
                     <button disabled={this.state.page <= 1} type='button' 
                       className='btn btn-dark' onClick={this.handlePrevClick}>&larr; Previous</button>
 
                     <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/12)} type='button' 
                       className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
-            </div>
+                */}
+            </>
         )
     }
 }
